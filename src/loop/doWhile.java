0fed8e43a79ccb09ecef45fb460bd4f22654f943
kostevg871 <kostyrkoevgeniy@yandex.ru>
2021-01-21 21:30:21 +0300

    do while learn

---
+++ src/loop/doWhile.java
@@ -0,0 +1,11 @@
+package loop;
+
+// example do while
+public class doWhile {
+    public static void main(String[] args) {
+        int a = 0;
+        do {
+            System.out.println("Hello");
+        } while (a > 0);
+    }
+}
