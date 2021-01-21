f47a5e1770b3111aa8b5416877429bcdd299380c
kostevg871 <kostyrkoevgeniy@yandex.ru>
2021-01-21 21:02:22 +0300

    lear loop

---
+++ src/loop/loop.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class loop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 1000) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
