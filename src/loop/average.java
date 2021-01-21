75b22bc5fa3710d1f3cfa2ce1f83af9f048a5998
kostevg871 <kostyrkoevgeniy@yandex.ru>
2021-01-21 21:25:14 +0300

    average

--- src/loop/average.java
+++ src/loop/average.java
@@ -1,4 +1,14 @@
 package loop;
 
 public class average {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+        while (i <= 100) {
+            sum += i;
+            i++;
+        }
+        float average = sum / (float)i;
+        System.out.println(average);
+    }
 }
