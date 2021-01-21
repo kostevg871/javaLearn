e114dcff2b13c535606ba5c069b30b44456d2e7d
kostevg871 <kostyrkoevgeniy@yandex.ru>
2021-01-21 21:34:44 +0300

    forLoop

---
+++ src/loop/forLoop.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class forLoop {
+    public static void main(String[] args) {
+        for (int i = 1000; i > 0; i--) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
