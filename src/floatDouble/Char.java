af6aa6c426c930cb78a81d4ec46e3fa2e2d9834b
kostevg871 <kostyrkoevgeniy@yandex.ru>
2021-01-21 20:30:26 +0300

    char

---
+++ src/floatDouble/Char.java
@@ -0,0 +1,9 @@
+package floatDouble;
+
+public class Char {
+    public static void main(String[] args) {
+        char ch1 = 89;
+        char ch2 = 'Y';
+        System.out.println("ch1 = " + ch1 + " ch2 = " + ch2);
+    }
+}
--- src/floatDouble/homeWork.java
+++ src/floatDouble/homeWork.java
@@ -5,7 +5,7 @@ public class homeWork {
     public static void main(String[] args) {
         float a = 3;
         float b = 4;
-        float c = (float) Math.sqrt(a*a + b*b);
+        float c = (float) Math.sqrt(a * a + b * b);
         System.out.println(c);
     }
 }
