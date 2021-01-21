c397b19f1930f6404f6e1f2d3ca8641d5d95fb71
kostevg871 <kostyrkoevgeniy@yandex.ru>
2021-01-21 21:53:24 +0300

    first arrays

---
+++ src/arrays/array.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class array {
+    public static void main(String[] args) {
+        int[] daysInMonth = new int[12];
+        daysInMonth[0] = 31;
+        daysInMonth[1] = 28;
+        daysInMonth[2] = 31;
+        daysInMonth[3] = 30;
+        daysInMonth[4] = 31;
+        daysInMonth[5] = 30;
+        daysInMonth[6] = 31;
+        daysInMonth[7] = 31;
+        daysInMonth[8] = 30;
+        daysInMonth[9] = 31;
+        daysInMonth[10] = 30;
+        daysInMonth[11] = 31;
+        for(int i=0; i< daysInMonth.length; i++){
+            System.out.println(daysInMonth[i]);
+        }
+    }
+}
