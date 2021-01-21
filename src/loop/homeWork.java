237fbd4ffceb57a4a5f6d88b7f75ba9faf12b118
kostevg871 <kostyrkoevgeniy@yandex.ru>
2021-01-21 21:37:31 +0300

    home work on cycles

---
+++ src/loop/homeWork.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class homeWork {
+    public static void main(String[] args) {
+        for (int i = 100; i <= 1000; i++) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
