0f0ce37340862ab9e2dc1dafebb47b11101830c1
kostevg871 <kostyrkoevgeniy@yandex.ru>
2021-01-21 20:18:31 +0300

    init

---
+++ src/floatDouble/areaOfCircle.java
@@ -0,0 +1,13 @@
+package floatDouble;
+
+// area of a circle
+public class areaOfCircle {
+    public static void main(String[] args) {
+        float r = 10.8f;
+        float pi = 3.14f;
+        float s = pi * r * r;
+        System.out.println("Площадь круга: " + s);
+    }
+}
+
+
---
+++ src/floatDouble/homeWork.java
@@ -0,0 +1,11 @@
+package floatDouble;
+
+// Pythagorean theorem
+public class homeWork {
+    public static void main(String[] args) {
+        float a = 3;
+        float b = 4;
+        float c = (float) Math.sqrt(a*a + b*b);
+        System.out.println(c);
+    }
+}
